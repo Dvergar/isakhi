665fee551beffa386966fb7c7737cd102bac1323
Dvergar <laurent.rd@gmail.com>
2020-03-31 18:38:51 +0200

    first commit

---
+++ lib/entity_manager.dart
@@ -0,0 +1,105 @@
+import 'dart:collection';
+
+class EntitySet {
+  EntityManager em;
+  Set<Type> code;
+  Set<Entity> _adds = Set();
+  Set<Entity> _changes = Set();
+  Set<Entity> _removes = Set();
+  Set<Entity> adds = Set();
+  Set<Entity> changes = Set();
+  Set<Entity> removes = Set();
+
+  EntitySet(this.em, this.code);
+
+  applyChanges() {
+    adds = _adds;
+    changes = _changes;
+    removes = _removes;
+
+    _adds = Set();
+    _changes = Set();
+    _removes = Set();
+  }
+}
+
+class Entity {
+  int id;
+  Set<Type> code = Set();
+
+  HashMap<Type, Component> components = HashMap();
+
+  Entity(this.id);
+}
+
+class EntityManager {
+  int ids = 0;
+
+  List<EntitySet> entitySets = List();
+
+  createEntity() {
+    var entityId = ids++;
+    return Entity(entityId);
+  }
+
+  destroyEntity(Entity entity) {
+    for (var entitySet in entitySets) {
+      for (var componentType in entity.components.keys) {
+        if (entitySet.code.contains(componentType)) {
+          entitySet._removes.add(entity);
+          break;
+        }
+      }
+    }
+  }
+
+  addComponent(Entity entity, Component instance) {
+    entity.components[instance.runtimeType] = instance;
+
+    for (var entitySet in entitySets) {
+      if (entitySet.code.containsAll(entity.code)) {
+        entitySet._adds.add(entity);
+      }
+    }
+  }
+
+  Component getComponent(Entity entity, Type componentClass) {
+    if (!entity.components.containsKey(componentClass))
+      throw new Exception("No component attached to this entity");
+
+    return entity.components[componentClass];
+  }
+
+  removeComponent(Entity entity, Type componentClass) {
+    for (var entitySet in entitySets) {
+      if (entitySet.code.contains(componentClass)) {
+        entitySet._removes.add(entity);
+      }
+    }
+  }
+
+  EntitySet getEntitySet(List<Type> code) {
+    var entitySet = EntitySet(this, code.toSet());
+    entitySets.add(entitySet);
+    return entitySet;
+  }
+
+  markChanged(Entity entity, Component component, [EntitySet filterEntitySet]) {
+    for (var entitySet in entitySets) {
+      if (entitySet == filterEntitySet) continue;
+      if (entitySet.code.contains(component.runtimeType)) {
+        entitySet._changes.add(entity);
+      }
+    }
+  }
+}
+
+abstract class Component {}
+
+abstract class System {
+  EntityManager em;
+
+  System(this.em);
+
+  loop();
+}
---
+++ test/test.dart
@@ -0,0 +1,114 @@
+import 'package:isakhi/entity_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('getComponent returns proper component', () {
+    var em = EntityManager();
+    var entity = em.createEntity();
+    var pos = PositionComponent(2, 4);
+    em.addComponent(entity, pos);
+    PositionComponent pos2 = em.getComponent(entity, PositionComponent);
+
+    expect(pos2.x, equals(pos.x));
+    expect(pos2.y, equals(pos.y));
+  });
+
+  test('Adds', () {
+    var em = EntityManager();
+    var entitySet = em.getEntitySet([PositionComponent, NameComponent]);
+    var entity = em.createEntity();
+    var pos = PositionComponent(2, 4);
+    em.addComponent(entity, pos);
+
+    entitySet.applyChanges();
+
+    expect(entitySet.adds.length, equals(1));
+  });
+
+  test('Changes', () {
+    var em = EntityManager();
+    var entitySet = em.getEntitySet([PositionComponent]);
+    var entity = em.createEntity();
+    var pos = PositionComponent(1, 2);
+    em.addComponent(entity, pos);
+
+    pos.x = 12;
+    em.markChanged(entity, pos);
+
+    entitySet.applyChanges();
+
+    expect(entitySet.changes.length, equals(1));
+  });
+
+  test('Removes', () {
+    var em = EntityManager();
+    var entitySet = em.getEntitySet([PositionComponent]);
+    var entity = em.createEntity();
+    var pos = PositionComponent(1, 2);
+    em.addComponent(entity, pos);
+    em.removeComponent(entity, PositionComponent);
+
+    entitySet.applyChanges();
+
+    expect(entitySet.removes.length, equals(1));
+  });
+
+  test('DestroyEntity', () {
+    var em = EntityManager();
+    var entity = em.createEntity();
+    var entitySet = em.getEntitySet([PositionComponent]);
+    var entitySet2 = em.getEntitySet([PositionComponent, NameComponent]);
+    var pos = PositionComponent(1, 2);
+    em.addComponent(entity, pos);
+    em.destroyEntity(entity);
+
+    entitySet.applyChanges();
+    entitySet2.applyChanges();
+
+    expect(entitySet.removes.length, 1);
+    expect(entitySet2.removes.length, 1);
+  });
+
+  // SYSTEM BEHAVIOUR
+  var em = EntityManager();
+  var positionSystem = PositionSystem(em);
+  var entity = em.createEntity();
+  var pos = PositionComponent(1, 2);
+
+  em.addComponent(entity, pos);
+  pos.x = 5;
+  em.markChanged(entity, pos);
+  em.destroyEntity(entity);
+
+  positionSystem.positionEntitySet.applyChanges();
+  positionSystem.loop();
+}
+
+class PositionComponent implements Component {
+  int x;
+  int y;
+
+  PositionComponent(this.x, this.y);
+}
+
+class NameComponent implements Component {
+  String text;
+
+  NameComponent(this.text);
+}
+
+class PositionSystem extends System {
+  EntitySet positionEntitySet;
+
+  PositionSystem(EntityManager em) : super(em) {
+    this.positionEntitySet = em.getEntitySet([PositionComponent]);
+  }
+
+  loop() {
+    test('System entitysets behaviour', () {
+      expect(positionEntitySet.adds.length, 1);
+      expect(positionEntitySet.changes.length, 1);
+      expect(positionEntitySet.removes.length, 1);
+    });
+  }
+}
